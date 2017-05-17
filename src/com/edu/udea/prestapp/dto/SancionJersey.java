748c107e9bfa1d8098ae590ee3d00fb8c7a1491f
Reizzel <cbp453252.hdrl@gmail.com>
2017-05-17 18:08:09 -0500

    SancionBL, SancionJersey, ReservaWS

--- src/com/edu/udea/prestapp/dto/SancionJersey.java
+++ src/com/edu/udea/prestapp/dto/SancionJersey.java
@@ -1,5 +1,57 @@
 package com.edu.udea.prestapp.dto;
 
-public class SancionJersey {
+import java.util.Date;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement 
+public class SancionJersey {
+	//La sancion se aplica a los usuarios que han prestado objetos, hay varios tipos de sanciones
+	private int id;
+	private Usuario usuario;
+	private String tipoSancion;
+	private Date inicioSancion;
+	private Date finSancion;
+	
+	public SancionJersey(){};
+	
+	public SancionJersey(int id, Usuario usuario, String tipoSancion, Date inicioSancion, Date finSancion) {
+		this.id = id;
+		this.usuario = usuario;
+		this.tipoSancion = tipoSancion;
+		this.inicioSancion = inicioSancion;
+		this.finSancion = finSancion;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public String getTipoSancion() {
+		return tipoSancion;
+	}
+	public void setTipoSancion(String tipoSancion) {
+		this.tipoSancion = tipoSancion;
+	}
+	public Date getInicioSancion() {
+		return inicioSancion;
+	}
+	public void setInicioSancion(Date inicioSancion) {
+		this.inicioSancion = inicioSancion;
+	}
+	public Date getFinSancion() {
+		return finSancion;
+	}
+	public void setFinSancion(Date finSancion) {
+		this.finSancion = finSancion;
+	}
 }
--- src/com/edu/udea/prestapp/ws/ReservaWS.java
+++ src/com/edu/udea/prestapp/ws/ReservaWS.java
@@ -1,5 +1,63 @@
 package com.edu.udea.prestapp.ws;
 
-public class ReservaWS {
+import java.util.Date;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.edu.udea.prestapp.bl.ReservaBL;
+import com.edu.udea.prestapp.exception.ExceptionController;
+import com.google.gson.Gson;
+
+@Path("reserva")
+@Component
+public class ReservaWS {
+	
+	@Autowired
+	ReservaBL reservaBL;
+	
+	@PUT
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("reserva")
+	public String reservar(@QueryParam("usuario")String usuario, @QueryParam("idObjeto")int idObjeto, @QueryParam("fechaPrestamo")Date fechaPrestamo){
+		try{
+			reservaBL.reservarObjeto(usuario, idObjeto, fechaPrestamo);
+			return "Reserva realizada";
+		}catch(ExceptionController e) {
+			return e.getMessage();
+		}
+	}
+	
+	@DELETE
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("cancelarReserva/{id}")
+	public String cancelar(@QueryParam("id")int id, @QueryParam("usuario")String usuario){
+		try{
+			reservaBL.cancelarReserva(id, usuario);
+			return "Reserva cancelada";
+		}catch(ExceptionController e) {
+			return e.getMessage();
+		}
+	}
+	
+	@PUT
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("modificarReserva")
+	public String modificar(@QueryParam("id")int id, @QueryParam("usuario")String usuario, @QueryParam("nuevaFecha")Date nuevaFecha){
+		try{
+			reservaBL.modificarReserva(id, usuario, nuevaFecha);
+			return "Reserva modificada";
+		}catch(ExceptionController e) {
+			return e.getMessage();
+		}
+	}
+	
+	
 }
